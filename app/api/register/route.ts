bf9567778456d366de7b2522fdf8871770963eb6
Habibullah <0226.mochhabiballah@gmail.com>
2026-07-17 10:33:59 +0700

    fix

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,22 +4,26 @@ export async function POST(req: Request) {
     try {
         const body = await req.json();
 
-        const response = await fetch(
-            process.env.NEXT_PUBLIC_APPS_SCRIPT_URL!,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(body),
-            }
-        );
+        const scriptUrl = process.env.GOOGLE_SCRIPT_URL;
+
+        if (!scriptUrl) {
+            throw new Error("GOOGLE_SCRIPT_URL belum tersedia");
+        }
+
+        const response = await fetch(scriptUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
 
         const data = await response.json();
 
         return NextResponse.json(data);
+
     } catch (error) {
-        console.error(error);
+        console.error("API REGISTER ERROR:", error);
 
         return NextResponse.json(
             {
