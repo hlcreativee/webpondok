970c41dcf7317b4899ac9831e53da00b1f11ab64
Habibullah <0226.mochhabiballah@gmail.com>
2026-07-23 11:37:07 +0700

    theme

--- providers/AppProviders.tsx
+++ providers/AppProviders.tsx
@@ -17,7 +17,7 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
         {children}
       </ThemeProvider>
     </QueryClientProvider>
